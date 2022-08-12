7fc8019cc0562daccb3e000aaea42ec441292cb7
emraza1 <47735258+emraza1@users.noreply.github.com>
2022-08-12 23:55:17 +0500

    Update footer.js

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -74,24 +74,17 @@ const FooterContent = `
   <div>
     <div id="footer">
     <p class="lead">
-      3. Export the project
+      3. Publish & Explore
     </p>
 
-    <p class="paragraph">
-        Your project is going to be safely hosted on GitHub - a space for code repositories online.
-    </p>
-    <p class="paragraph">
-    If you would like to save your project locally, you can also download the package containing the generated code and all supporting files.
-    </p>
+   
 
     <div class="buttons">
       <button id="github-publish" disabled class="primary-button publish-disabled">
-        Publish on Github
+        Publish
       </button>
 
-      <button id="zip-publish" disabled class="passive-button publish-disabled">
-        Download package
-      </button>
+
     </div>
     </div>
   </div>`;
